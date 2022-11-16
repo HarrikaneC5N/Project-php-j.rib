ad80a022e8276ab0cefbde061588076fe60adedd
Cedric <c.louchard@it-students.fr>
2022-11-16 16:12:37 +0100

    Ajout du script permettant l'affichage du role selectionner grace à un formulaire html

--- exo7/index.php
+++ exo7/index.php
@@ -1,3 +1,25 @@
+<?php
+
+$roles = ["Admin", "Membre", "Gestionnaire", "Autre"];
+    function role(){
+        if(isset($_POST['role'])){
+            if($_POST['role']=='Admin'){
+                echo "<p> Vous êtes l'Administrateur du site.</p>";
+            }
+            if($_POST['role']=='Membre'){
+                echo "<p> Vous êtes membre du site. </p>";
+            }
+            if($_POST['role']=='Gestionnaire'){
+                echo "<p> Vous êtes gestionnaire du site. </p>";
+            }
+            if($_POST['role']=='Autre'){
+                echo "<p> Bienvenu invité </p>";
+            }
+        }else{
+            echo "<p>Vous n'avez actuellement pas de rôle</p>";
+        };
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +29,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class ="container">
+        <div>
+            <?php
+                role();
+            ?>
+        </div>
+        <div>
+            <p>Veuillez choisir un rôle</p>
+        </div>
+        <form action="" method="post">
+            <select name="role" id="role">
+                <option value="">Veuillez selectionner un rôle</option>
+                <?php foreach ($roles as $key => $role){ ?>
+                <option value="<?php echo $role; ?>"><?php echo $role; ?></option>
+                <?php }; ?>
+            </select>
+            <input type="submit">
+        </form>
+    </div>
 </body>
 </html>
