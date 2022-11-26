96ceed74301ae5827ce767c3ec54e5bb4d222eaa
Cedric <c.louchard@it-students.fr>
2022-11-26 16:15:47 +0100

    ajout des .idea jsp a quoi ça sert mais bon

---
+++ exoAlgo/exo3/algoFindLetter.php
@@ -0,0 +1,61 @@
+<?php
+
+session_start();
+
+$word = $_SESSION['word'] ='Autruche';
+// $arrayToFind [] = $_SESSION['toFind'] = $toFind;
+$toFind[] = [];
+
+if(isset($_POST['letter'])){
+    $letter = $_SESSION['letter'] = $_POST['letter'];
+    $wordArray []= $_SESSION['wordArray']= [];
+    $try = $_SESSION['try'] = 10;
+    
+    echo "Le mot : ";
+    // initialisation du tableau
+    for($i = 0; $i < strlen($word); $i++){
+        $wordArray = $word[$i];
+        $toFind = "_ ";
+    }
+    
+}
+elseif(!isset($_POST['letter'])){
+    echo "Le mot : ";
+    // initialisation du tableau
+    for($i = 0; $i < strlen($word); $i++){
+        $wordArray = $word[$i];
+        $toFind = "_ ";
+    }
+}
+var_dump($_SESSION);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+<form action="" method="post" >
+        <label for="letter"> Essayez une lettre : </label>
+        <input type="text" name="letter" id="letter" minlength="1" maxlength="2" required>
+        <input name="letter" type="text"  minlength="1" maxlength="2" required>
+        <input type="submit" value=" verifier ">
+</form>
