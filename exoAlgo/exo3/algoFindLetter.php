4a33c409c26d51a26ece7432498548105395e695
Cedric <c.louchard@it-students.fr>
2022-11-27 12:23:51 +0100

    update

--- exoAlgo/exo3/algoFindLetter.php
+++ exoAlgo/exo3/algoFindLetter.php
@@ -6,7 +6,6 @@ $word = $_SESSION['word'] = 'Autruche';
 // $arrayToFind [] = $_SESSION['toFind'] = $toFind;
 $toFind[] = [];
 
-if (isset($_POST['letter'])) {
     $letter = $_SESSION['letter'] = $_POST['letter'];
     $wordArray [] = $_SESSION['wordArray'] = [];
     $try = $_SESSION['try'] = 10;
@@ -17,12 +16,4 @@ if (isset($_POST['letter'])) {
         $wordArray = $word[$i];
         $toFind = "_ ";
     }
-} elseif (!isset($_POST['letter'])) {
-    echo "Le mot : ";
-    // initialisation du tableau
-    for ($i = 0; $i < strlen($word); $i++) {
-        $wordArray = $word[$i];
-        $toFind = "_ ";
-    }
-}
 ?>
