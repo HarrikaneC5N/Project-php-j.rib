98480b45b510f0ec743b5fda1ecf11331f742054
Cedric <c.louchard@it-students.fr>
2022-11-29 21:25:16 +0100

    suppression double form

--- exoAlgo/exo3/index.php
+++ exoAlgo/exo3/index.php
@@ -3,6 +3,7 @@
 session_start();
 
 $word = $_SESSION['word'] ='Autruche';
+
 // $arrayToFind [] = $_SESSION['toFind'] = $toFind;
 $toFind[] = [];
 
@@ -10,25 +11,17 @@ if(isset($_POST['letter'])){
     $letter = $_SESSION['letter'] = $_POST['letter'];
     $wordArray []= $_SESSION['wordArray']= [];
     $try = $_SESSION['try'] = 10;
-    
-    echo "Le mot : ";
+
     // initialisation du tableau
     for($i = 0; $i < strlen($word); $i++){
         $wordArray = $word[$i];
-        $toFind = "_ ";
+        $toFind[] = "_ ";
     }
-    
 }
-elseif(!isset($_POST['letter'])){
-    echo "yo";
-}
-var_dump($_SESSION);
 
 ?>
-
 <form action="" method="post" >
         <label for="letter"> Essayez une lettre : </label>
         <input type="text" name="letter" id="letter" minlength="1" maxlength="2" required>
-        <input name="letter" type="text"  minlength="1" maxlength="2" required>
         <input type="submit" value=" verifier ">
 </form>
